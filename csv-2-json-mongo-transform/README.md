957d8fd030515b901f99351ff1af3f4159d6b240
Shridhar SD <Shridhar@koolnano.tech>
2025-09-22 12:34:53 +0530

    Added , fixed test code, created argo-demo-folder

--- csv-2-json-mongo-transform/README.md
+++ csv-2-json-mongo-transform/README.md
@@ -5,4 +5,8 @@
 ## TSV to
 
 ## run the test
+
+```
+cd csv-2-json-mongo-transform
 python -m unittest TestDataProcessor.py -v
+```
--- csv-2-json-mongo-transform/TestDataProcessor.py
+++ csv-2-json-mongo-transform/TestDataProcessor.py
@@ -9,12 +9,12 @@ class TestDataProcessor(unittest.TestCase):
     def setUp(self):
         """Setup before each test"""
         self.processor = DataProcessor()
-        self.input_file = os.path.join("processed_data", "input.tsv")
+        self.input_file = os.path.join("data", "csv-2-json-mongo-transform-input.tsv")
         self.collection_name = "test_stock_data"
 
         # Output files
-        self.output_tsv_jumbled = os.path.join("processed_data", "output_jumbled.tsv")
-        self.output_json_original = os.path.join("processed_data", "output.json")
+        self.output_tsv_jumbled = os.path.join("data", "output_jumbled.tsv")
+        self.output_json_original = os.path.join("data", "output.json")
 
     def test_file_read(self):
         """Test reading TSV file"""
@@ -25,7 +25,7 @@ class TestDataProcessor(unittest.TestCase):
     def test_save_original_json(self):
         """Save original TSV data as JSON (no jumble)"""
         data = self.processor.file_read(self.input_file)
-        os.makedirs("processed_data", exist_ok=True)
+        os.makedirs("data", exist_ok=True)
         with open(self.output_json_original, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=2, ensure_ascii=False)
         print(f"✅ Original input saved as JSON '{self.output_json_original}'")
